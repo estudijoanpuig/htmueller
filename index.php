179dded0cbffe23bb37b0a943128da6051d9a6e7
estudijoanpuig <joanpuigbertran@gmail.com>
2025-05-07 12:39:42 +0200

    bb

--- index.php
+++ index.php
@@ -567,6 +567,10 @@ $conn->close();
 
                 <div class="row s-testimonials__content">
                     <div class="column lg-12">
+					
+					
+					
+					
 
                          <!-- NANOGALLERY-->              
 <div class="uk-container uk-text-center uk-padding">
@@ -575,7 +579,7 @@ $conn->close();
 		<link href="https://unpkg.com/nanogallery2/dist/css/nanogallery2.min.css" rel="stylesheet" type="text/css">
 		<div id="nanogallery2" data-nanogallery2='{
 		"kind": "nano_photos_provider2",
-		"dataProvider": "http://htmueller.test/nanogallery/nano_photos_content",
+		"dataProvider": "http://htmueller.test/nano_photos_provider2.php",
 		"ThumbnailHeight": 350,
 		"ThumbnailWidth": 350
 		}'>
